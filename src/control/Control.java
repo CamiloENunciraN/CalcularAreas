e7931ccbd7b4f896fe6c7e5741a96bbab5781c18
Camilo <camiloeduardonn@ufps.edu.co>
2022-09-26 09:38:21 -0500

    clases implement

---
+++ src/control/Control.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package control;
+
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Control implements ActionListener {
+private Circulo    ;
+private Rectangulo;    
+private Triangulo; 
+private Vista;  
+
+
+
+
+
+}
---
+++ src/modelo/Circulo.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Circulo extends FiguraGeometrica {
+
+
+    private Integer radio;
+
+    public Circulo(Integer radio) {
+        this.radio = radio;
+    }
+
+    public Circulo() {
+    }
+
+@Override
+public Double area(){
+return 
+
+}
+
+
+}
